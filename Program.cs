24a031a3433b21decfe32c5ac59215d040af31bf
Pankaj Dubey <pankaj_dubey30@yahoo.com>
2025-12-25 11:55:36 +0530

    Add new classs Person and changes in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a sample C# program.");
+Console.WriteLine("This is a sample C# program.");
+Console.WriteLine("This is a sample C# program.");
